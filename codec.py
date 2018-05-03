9a90024339a8daf64df9cb9b52741e021a10dbd7
cepin <cepin19@gmail.com>
2018-05-04 01:39:39 +0200

    trochu to funguje, ale moc ne

--- codec.py
+++ codec.py
@@ -5,7 +5,7 @@ import soundfile as sf
 from scipy.signal import hann, lfilter, freqz, decimate, butter
 from scipy.io.wavfile import read as wavread
 from scipy.io.wavfile import write as wavwrite
-from scipy.signal import hann, lfilter, freqz, decimate, butter
+from scipy.signal import hann, lfilter, freqz, decimate, butter, medfilt
 from numpy import array, double, amax, absolute, zeros, floor, arange, mean
 from numpy import correlate, dot, append, divide, argmax, int16, sqrt, power,corrcoef,std
 from numpy.random import randn
@@ -24,10 +24,12 @@ clusters=kmeans2(data, 10)
 #print (clusters)
 print (data.shape)
 
+
+
 def voiced(c):
     min_s, max_s= 20,160
 
-    is_voiced = max(c[min_s:max_s]) > 0.25*c[0] # znelost se urcuje jako max(R)>a.R(0), a muze byt treba 0.1
+    is_voiced = max(c[min_s:max_s]) > 0.15*c[0] # znelost se urcuje jako max(R)>a.R(0), a muze byt treba 0.1
     return is_voiced
 def get_f0(c):
 
@@ -35,61 +37,154 @@ def get_f0(c):
     #rozsah lagu ve vzorcich, pri 8000Hz odpovida f 50-400Hz
     min_s, max_s= 20,160
     #vyberu nejvyssi autokorelacni koeficient
-    pitch = argmax(c[min_s:max_s])
+    pitch = argmax(c[min_s:max_s])+min_s
     #print(c)
 
     return pitch
+def load_file(fn):
+    out=[]
+    with open(fn) as infile:
+        data=infile.read().split(" ")
+        for idx in xrange(0,len(data)-14,13): #10*lpc+e+f0
+            out.append([float(d) for d in data[idx:idx+11]])
+            out.append([float(data[idx+11])])
+            out.append(int(float(data[idx+12])))
+           # print ("aa")
+            #print (numpy.asarray(data[idx:idx+10],dtype='float32'))
+            #print ("e")
+            #print ([float(data[idx+11])])
+            #print ("f0")
+            #print (int(float(data[idx+12])))
+    return out#numpy.asarray(out,dtype='float32')
 
-b, aa = butter(1, 200/8000., 'high')
+def decode(enc):
+    # axs.grid(True)
+    #print(len(enc))
+    b, aa = butter(1, 800 / 8000., 'high')
 
-out=zeros(len(data)*2)
-for idx, block in enumerate(sf.blocks('test.wav', blocksize=256, overlap=128)):
-    glottal_lowpass = lambda signal: lfilter(b, aa, block)
+    out=zeros(len(data) * 2)
+    for idx in xrange(0,len(enc),3):
+        #print(idx)
+        a=numpy.asarray(enc[idx])
+        e=numpy.asarray(enc[idx+1])
+        #numpy.ndarray((1,),buffer=enc[idx+1],dtype='float32')
+        f0_sam=enc[idx+2]
 
-    #odstraneni SS
+        #print (a)
+        #print (e)
+        #print (f0_sam)
 
-    block-=mean(block)
-    #spocitani autokorelacnich koeficientu
-    block = (block - mean(block)) / (std(block) * len(block))
-    #autokorelacni koeficienty
-    c=(correlate(block, block, mode='full'))
-    c = c[len(c)/2:]
-    f0_sam=get_f0(block)
-    is_voiced=voiced(c)
-    f0_freq=1/(f0_sam/8000.)
-    print (idx)
-    print (f0_sam)
-    print (f0_freq)
-    print (is_voiced)
-    a,e,_=lpc(block,10)
-    e=e**2
-    print (a)
-    print ("______________________")
-
-    #fig, axs = plt.subplots(1, 1)
-    #axs.plot(block)
-
-    #axs.grid(True)
-    if is_voiced and f0_sam!=0:
-        vocoded = zeros(len(block))
-        vocoded[f0_sam::f0_sam] = 1.0
-        vocoded = glottal_lowpass(vocoded)
-    else:
-        vocoded = randn(len(block))/2
+        if f0_sam != 0:
+            print ("voiced")
+            vocoded = zeros(256)
+            #f0_sam=
+            vocoded[f0_sam::f0_sam] = 1.0
+            #???
+#            vocoded =lfilter(b, aa,vocoded)
+        else:
+            print ("unvoiced")
+            vocoded = randn(256)
         vocoded = lfilter(e, a, vocoded)
-        vocoded *= hann(len(block))
-    try:
-        out[idx*256:idx*256+256] += vocoded
-    except Exception as e:
-        print (e)
+        vocoded *= hann(256)
+        #print (vocoded.shape)
+        #print (out.shape)
 
-    #return out
-    #fig.tight_layout()
-    #plt.show()
-    #print (lpc(block,10))
+        try:
+          #  print ("starti %s"%(idx/3 * 128))
+         #   print ("endi %s"%((idx/3 * 128)+256))
 
-print(array(out) * (2**15-1))
+            out[idx/3 * 128:idx/3 * 128 + 256] += vocoded
+        except Exception as e:
+            print (e)
 
-wavwrite('vocoded.wav', 8000, array(out/amax(absolute(out)) * (2**15-1), dtype=int16))
+    return out
+            # fig.tight_layout()
+            # plt.show()
+            # print (lpc(block,10))
+
+
+def encode(fn):
+    out=zeros(len(data)*2)
+    out=[]
+    pitches=[]
+    for idx, block in enumerate(sf.blocks(fn, blocksize=256, overlap=128)):
+        gain_correction = (1 - 128) * 2  # *2 due to hann window
+        block *= hann(len(block)) * gain_correction
+        #odstraneni SS
+
+        block-=mean(block)
+        #spocitani autokorelacnich koeficientu
+        #autokorelacni koeficienty
+        c=(correlate(block, block, mode='full'))
+        c = c[len(c)/2:]
+        f0_sam=get_f0(c)
+        pitches.append(f0_sam)
+        print ("old")
+        print f0_sam
+        f0_sam=medfilt(pitches,kernel_size=3)[-1]
+        print ("new")
+        print f0_sam
+
+        is_voiced=voiced(c)
+
+        f0_freq=1/(f0_sam/8000.)
+
+
+
+
+        if not is_voiced:
+            f0_sam=0
+            f0_freq=0
+
+       # print (idx)
+       # print (f0_sam)
+       # print (f0_freq)
+       # print (is_voiced)
+       # block = preemphasis(block)
+   
+
+        a,e,_=lpc(block,10)
+       # e=math.sqrt(e**2)
+        #print (a)
+        #print (e)
+
+       # e = rms(lfilter(a, (1,), block))
+
+
+
+        #print ("______________________")
+        out.append(a)
+        out.append(e)
+        out.append(f0_sam)
+        #fig, axs = plt.subplots(1, 1)
+        #axs.plot(block)
+    return numpy.asarray(out)
+
+#print(array(out) * (2**15-1))
+enc=encode("test.wav")
+with (open("out.cod",'w')) as outfile:
+    for idx in xrange(0, len(enc), 3):
+        print(idx)
+        a = enc[idx]
+        for aa in a:
+            outfile.write( "{:.9f} ".format(float(aa)))
+
+        e = enc[idx + 1]
+        outfile.write("{:.9f} ".format(float(e)))
+
+        f0_sam = enc[idx + 2]
+        outfile.write(str(f0_sam) +" ")
+        outfile.write("\n")
+
+       # print ("a")
+       # print (a)
+       # print ("e")
+       # print (e)
+       # print ("f0")
+       # print (f0_sam)
+#print (load_file("out.cod"))
+out=decode(load_file("out.cod"))
+#print (load_file("out.cod"))
+wavwrite('vocoded.wav', 8000, array(out*2/amax(absolute(out)) * (2**15-1), dtype=int16))
 
 
